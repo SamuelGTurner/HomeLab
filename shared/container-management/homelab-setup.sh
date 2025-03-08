0372794071cb6e2aa6ab7b08692bbdc3335737f4
Sam Turner <SamuelGTurner@users.noreply.github.com>
2025-03-08 23:04:46 +0000

    Update homelab-setup.sh

--- shared/container-management/homelab-setup.sh
+++ shared/container-management/homelab-setup.sh
@@ -26,9 +26,30 @@ cleanup() {
     rm -rf "$TMP_DIR"
 }
 
+# Docker Prerequisites
+enable_docker_prerequisites() {
+    for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do apt remove -y $pkg; done
+
+    local deps="ca-certificates curl"
+    log "INFO" "Installing build dependencies: $deps"
+    apt install -y $deps || error_exit "Failed to install docker prerequisites"
+
+    # Add Docker's official GPG key:
+    install -m 0755 -d /etc/apt/keyrings
+    curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+    chmod a+r /etc/apt/keyrings/docker.asc
+
+    # Add the repository to Apt sources:
+    echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+    $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+    tee /etc/apt/sources.list.d/docker.list > /dev/null
+    apt update
+    }
+
 # Install dependencies
 install_dependencies() {
-    local deps="git cmake build-essential pkg-config libzmq3-dev libssl-dev zlib1g-dev libpq-dev unzip libusb-1.0-0-dev libsqlite3-dev"
+    local deps="docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin"
     log "INFO" "Installing build dependencies: $deps"
     apt install -y $deps || error_exit "Failed to install dependencies"
 }
@@ -40,6 +61,9 @@ main() {
     trap cleanup EXIT
 
     apt update || error_exit "Failed to update package lists"
+
+    enable_docker_prerequisites
+    install_dependencies
 
     # Launch Container Mangement Stack
     # wget -qO- https://raw.githubusercontent.com/SamuelGTurner/HomeLab/main/shared/container-management/docker-compose.yml | docker compose -f - up -d
