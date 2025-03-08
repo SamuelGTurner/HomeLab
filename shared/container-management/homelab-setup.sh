34987e5d6a28d1317f423c8debe41352d0559a29
Sam Turner <SamuelGTurner@users.noreply.github.com>
2025-03-08 22:44:14 +0000

    Update homelab-setup.sh

--- shared/container-management/homelab-setup.sh
+++ shared/container-management/homelab-setup.sh
@@ -26,13 +26,20 @@ cleanup() {
     rm -rf "$TMP_DIR"
 }
 
+# Install dependencies
+install_dependencies() {
+    local deps="git cmake build-essential pkg-config libzmq3-dev libssl-dev zlib1g-dev libpq-dev unzip libusb-1.0-0-dev libsqlite3-dev"
+    log "INFO" "Installing build dependencies: $deps"
+    apt install -y $deps || error_exit "Failed to install dependencies"
+}
+
 # Main function
 main() {
     log "INFO" "Starting HomeLab Container Management installation script"
 
     trap cleanup EXIT
 
-    sudo apt-get update || error_exit "Failed to update package lists"
+    apt update || error_exit "Failed to update package lists"
 
     # Launch Container Mangement Stack
     # wget -qO- https://raw.githubusercontent.com/SamuelGTurner/HomeLab/main/shared/container-management/docker-compose.yml | docker compose -f - up -d
