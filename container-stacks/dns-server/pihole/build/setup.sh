cd06435212763b0dbbe2dd7f62b40556e7355128
Sam Turner <SamuelGTurner@users.noreply.github.com>
2025-04-21 16:36:58 +0100

    Update setup.sh

--- container-stacks/dns-server/pihole/build/setup.sh
+++ container-stacks/dns-server/pihole/build/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts" >> /etc/pihole/adlists.list
+echo "https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts" > /etc/pihole/adlists.list
 echo "https://v.firebog.net/hosts/Easylist.txt" >> /etc/pihole/adlists.list
 echo "https://big.oisd.nl" >> /etc/pihole/adlists.list
 echo "https://raw.githubusercontent.com/hagezi/dns-blocklists/main/adblock/pro.txt" >> /etc/pihole/adlists.list
